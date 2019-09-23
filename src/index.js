52af42cd81c6cf9e48fd7cb7c033efdb8b904891
Vladislav <>
2019-09-23 09:20:11 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,19 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let lettersArr = [];
+    let resultStr = '';
+    let exprArr = expr.split('');
+    for (let i = 0; i < exprArr.length; i++) {
+      if(exprArr[i] === '1' && exprArr[i + 1] === '0') lettersArr.push('.');
+      else if(exprArr[i] === '1' && exprArr[i + 1] === '1') lettersArr.push('-');
+      i++;
+      if ((i + 1) % 10 === 0){
+        resultStr += lettersArr.join('') === '' ? ' ' : MORSE_TABLE[lettersArr.join('')] ;
+        lettersArr = [];
+      }
+    }
+    return resultStr;
 }
 
 module.exports = {
